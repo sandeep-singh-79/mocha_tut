afe2239b947e003bc5538e380c76ae17c24b6214
Sandeep Singh <s.sandeep79@ymail.com>
2020-12-09 15:03:16 +0530

    api server test code complete

--- converter/test/server.js
+++ converter/test/server.js
@@ -1,14 +1,37 @@
-var expect = require("chai").expect
-var request = require("request")
+var expect = require("chai").expect;
+var request = require("request");
 
-describe("Color Code Converter API", function() {
-    describe("RGB to Hex conversion", () => {
-        it("returns status 200", () => {})
-        it("returns the color in hex", () => {})
-    })
+describe("Color Code Converter API", function () {
+  describe("RGB to Hex conversion", () => {
+    var url = "http://localhost:3000/rgbToHex?red=255&green=255&blue=255";
+    // passing done to enable mocha to wait for some of the expectations (requests in this case)
+    it("returns status 200", (done) => {
+      request(url, function (error, response, body) {
+        expect(response.statusCode).to.equal(200);
+        done();
+      });
+    });
+    it("returns the color in hex", (done) => {
+      request(url, function (error, response, body) {
+        expect(body).to.equal("ffffff");
+        done()
+      });
+    });
+  });
 
-    describe("Hex to RGB conversion", () => {
-        it("returns status 200", () => {})
-        it("returns the color in RGB", () => {})
-    })
+  describe("Hex to RGB conversion", () => {
+    var url = "http://localhost:3000/hexToRgb?hex=00ff00";
+    it("returns status 200", (done) => {
+      request(url, function (error, response, body) {
+        expect(response.statusCode).to.equal(200);
+        done();
+      });
+    });
+    it("returns the color in RGB", (done) => {
+      request(url, function (error, response, body) {
+        expect(body).to.equal("[0, 255, 0]");
+        done();
+      });
+    });
+  });
 });
