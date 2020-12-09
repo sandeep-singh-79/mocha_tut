03fe284be5a814c2c7fb74700375505490d00763
Sandeep Singh <s.sandeep79@ymail.com>
2020-12-09 14:50:12 +0530

    added initial test code

---
+++ converter/test/server.js
@@ -0,0 +1,14 @@
+var expect = require("chai").expect
+var request = require("request")
+
+describe("Color Code Converter API", function() {
+    describe("RGB to Hex conversion", () => {
+        it("returns status 200", () => {})
+        it("returns the color in hex", () => {})
+    })
+
+    describe("Hex to RGB conversion", () => {
+        it("returns status 200", () => {})
+        it("returns the color in RGB", () => {})
+    })
+});
